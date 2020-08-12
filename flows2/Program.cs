07c8c2fdb143e7e60e78c7e1dc43e6365b2fb39d
Влад Зброжек <pointbreak22@outlook.com>
2020-08-12 17:44:15 +0300

    Исправлено последнее задание

--- flows2/Program.cs
+++ flows2/Program.cs
@@ -2,14 +2,14 @@
 using System.Threading.Tasks;
 using System.Threading;
 using System.Collections.Generic;
-
+using System.Reflection.Metadata;
 
 namespace flows2
 {
     public interface IJobExecutor
     {
         /// Кол-во задач в очереди на обработку
-         public int Amount { get; }
+        public int Amount { get; }
 
         /// Запустить обработку очереди и установить максимальное кол-во  параллельных задач
         public void Start(int maxConcurrent);
@@ -25,137 +25,88 @@ namespace flows2
     }
     public class Executor : IJobExecutor
     {
-        private static Queue<Action> QueueActions = new Queue<Action>(); //очередь потоков
-        private  static  CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
-        private   CancellationToken token = cancelTokenSource.Token;     
-        public int Amount { get { return QueueActions.Count; } }
+        private Queue<Action> _QueueActions = new Queue<Action>(); //очередь потоков     
+        private Semaphore _semaphore;
+        private Task _task;
+        private CancellationTokenSource _cancellationToken =new CancellationTokenSource();
+        private CancellationToken _token;
+        private  ManualResetEvent _me= new ManualResetEvent(false);   //прерывание
         public void Start(int maxConcurrent)
         {
-
-            Semaphore semaphore = new Semaphore(maxConcurrent, maxConcurrent); //установка лимита потоков          
-            cancelTokenSource.Cancel();
-            cancelTokenSource = new CancellationTokenSource();
-            token = cancelTokenSource.Token;
-            Task.Factory.StartNew(() => //запуск отдельного потока для выполнения задач
-            {             
-                Console.WriteLine("//Поток для обработки очереди включен//");
-                while (true) //цикл для ожидания задач пока токен активный
+            _semaphore = new Semaphore(maxConcurrent, maxConcurrent);    //установка лимита потоков  
+            _cancellationToken.Cancel();
+            _me.Set(); //игнор остановки
+            _task = new Task(method);      
+            _task.Start();
+        }    
+        public void method()
+        {
+            Console.WriteLine("//Поток для обработки очереди включен//");
+            _cancellationToken = new CancellationTokenSource();
+            _token = _cancellationToken.Token;
+            while (true) //цикл для ожидания задач пока токен активный
+            {
+                if (_token.IsCancellationRequested)
                 {
-                    if (token.IsCancellationRequested)
-                    {
-                        Console.WriteLine("//поток обработки очереди остановлен //");                
-                      
-                        break;
-                    }
-                    if (QueueActions.Count>0)
-                    {
-                        Task.Factory.StartNew(() =>
-                        {
-                            semaphore.WaitOne();
-                            if (QueueActions.Count > 0)
-                            {
-                                QueueActions.Dequeue()?.Invoke();
-                            }
-                            semaphore.Release();
-                        });
-                    }                                    
+                    Console.WriteLine("//поток обработки очереди остановлен //");
+                    break;
                 }
-            });
-        }                           
+                if (_QueueActions.Count > 0)
+                {
+                    Task.Factory.StartNew(() =>
+                    {
+                        _semaphore.WaitOne();
+                        if (_QueueActions.Count > 0)
+                        {
+                            _QueueActions.Dequeue()?.Invoke();
+                        }
+                        _semaphore.Release();
+                    });
+                }
+                else 
+                {
+                    Console.WriteLine("//Задачи отсутствуют//");
+                    _me.Reset();  //сбрасывание прерывания
+                    _me.WaitOne();//остановка
+                  
+                }
+            }
+        }
+        public int Amount { get { return _QueueActions.Count; } }
         public void Stop()
         {
-            cancelTokenSource.Cancel();
+            _cancellationToken.Cancel();
+            _me.Set();        
             Console.WriteLine("//Остановка потока обработки очереди//");
         }
         public void Add(Action action)
-        {
-            QueueActions.Enqueue(action); //добавление в очередь
-            Console.WriteLine("Добавлена в очерередь задача №" + QueueActions.Count);
+        {        
+            _QueueActions.Enqueue(action); //добавление в очередь
+            Console.WriteLine("Добавлена в очерередь задача №" + _QueueActions.Count);
+            _me.Set();             
         }
         public void Clear()
         {
-            QueueActions.Clear(); //очистка
+            _QueueActions.Clear(); //очистка
             Console.WriteLine("Текущие адачи очищены");
         }
-    }
-    public class Zadachi //класс с задачами
-    {
-        Executor executor = new Executor();
-        public void add1()
-        {
-            Task.Factory.StartNew(() =>
-            { 
-                for (int i=0; i<10;i++)
-                {
-                      Action action1 = () =>
-                      {
-                        
-                          Console.WriteLine("Задача из 1го потока id=" + Thread.GetCurrentProcessorId() + " началась");
-                          Thread.Sleep(200);
-                          Console.WriteLine("Задача из 1го потока id=" + Thread.GetCurrentProcessorId() + " завершилась");
-                      };
-                     executor.Add(action1);             
-                                }
-            });
-        }
-        public void add2()
-        {
-            Task.Factory.StartNew(() =>
-            {
-                for (int i = 0; i < 5; i++)
-                {
-                    Action action2 = () =>
-                    {                       
-                        Console.WriteLine("Задача из 2го потока id=" + Thread.GetCurrentProcessorId() + " началась");
-                        Thread.Sleep(1000);
-                        Console.WriteLine("Задача из 2го потока id=" + Thread.GetCurrentProcessorId() + " завершилась");
-                    };
-                    executor.Add(action2);
-                }
-            });
-
-        }
-        public void add3()
-        {
-            Task.Factory.StartNew(() =>
-            {
-                for (int i = 0; i < 3; i++)
-                {
-                    Action action3 = () =>
-                    {
-                    
-                        Console.WriteLine("Задача из 3го потока id=" + Thread.GetCurrentProcessorId() + " началась");
-                        Thread.Sleep(2000);
-                        Console.WriteLine("Задача из 3го потока id=" + Thread.GetCurrentProcessorId() + " завершилась");
-                    };
-                    executor.Add(action3);
-
-                }
-            });
-
-        }
-
     }
     class Program
     {
         static void Main(string[] args)
         {
-          
-         
+            CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
             Executor executor = new Executor();
-            Zadachi zadachi = new Zadachi();
             string str;
-            Console.WriteLine("Ввдедите \"start\" чтоб запустить обработку очереди, \"add1\" для добавления задач c первого потока, \"add2\" со второго потока,\"add3\" с третьего, \"clear\" очистить текущую очередь, \"Amout\" количество задач в очереди, \"stop\" остановить обработку,  \"exit\" выйти  ");
-
+            Console.WriteLine("Ввдедите \"start\" чтоб запустить обработку очереди, \"add_actions\" для добавления задач , \"clear\" очистить текущую очередь, \"Amout\" количество задач в очереди, \"stop\" остановить обработку,  \"exit\" выйти  ");
             while (true)
             {
-                
                 str = Console.ReadLine();
                 switch (str)
                 {
                     case "start":
                         Console.WriteLine("Введите количество паралельных задач");
-                        string s =Console.ReadLine();
+                        string s = Console.ReadLine();
                         if (int.TryParse(s, out int n))
                         {
                             executor.Start(n);
@@ -166,21 +117,46 @@ namespace flows2
                             Console.WriteLine("ввели не число, введите команду заново");
                         }
                         break;
-                    case "clear": executor.Clear();break;
-                    case "Amout": Console.WriteLine("Количество задач в очереди " + executor.Amount.ToString());break;
-                    case "stop":executor.Stop(); break;
-                    case "add1":zadachi.add1();break;
-                    case "add2": zadachi.add2(); break;
-                    case "add3": zadachi.add3(); break;
+                    case "clear": executor.Clear(); break;
+                    case "Amout": Console.WriteLine("Количество задач в очереди " + executor.Amount.ToString()); break;
+                    case "stop": executor.Stop(); break;
+                    case "add_actions":
+                        Thread thread1 = new Thread(_ =>
+                         {
+                             for (int i = 0; i < 10; i++)
+                             {
+                                 Action action1 = () =>
+                                 {
+                                     Console.WriteLine("Задача из 1го потока id=" + Thread.GetCurrentProcessorId() + " началась");
+                                     Thread.Sleep(100);
+                                     Console.WriteLine("Задача из 1го потока id=" + Thread.GetCurrentProcessorId() + " завершилась");
+                                 };
+                                 executor.Add(action1);
+                             }
+                         });
+                        thread1.Start();
+                        Thread thread2 = new Thread(_ =>
+                        {
+                            for (int i = 0; i < 5; i++)
+                            {
+                                Action action1 = () =>
+                                {
+                                    Console.WriteLine("Задача из 2го потока id=" + Thread.GetCurrentProcessorId() + " началась");
+                                    Thread.Sleep(500);
+                                    Console.WriteLine("Задача из 2го потока id=" + Thread.GetCurrentProcessorId() + " завершилась");
+                                };
+                                executor.Add(action1);
+                            }
+                        });
+                        thread2.Start();
+                        break;
                     default: Console.WriteLine("Команды не существует "); break;
-
-                }                                
+                }
                 if (str == "exit")
                 {
                     break;
                 }
-            }    
-                
+            }
             Console.WriteLine("Нажмите любую клавишу для закрытия");
             Console.ReadLine();
         }

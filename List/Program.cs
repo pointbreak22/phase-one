4bb36a44d35abf88c9abe78aacb56b4f79a515fb
Влад Зброжек <pointbreak22@outlook.com>
2021-05-01 17:51:06 +0300

    rename

--- List/Program.cs
+++ List/Program.cs
@@ -9,17 +9,17 @@ namespace List
         {
             Dictionary<int, Person> persons = new Dictionary<int, Person>
             {
-                { 1, new Person { Fio = "Петр", Date = DateTime.Parse("Jan 1, 2009"), PlaceOfBirt = "Западный", PassportNumber = 123, PlaceOfWork = "Cтроитель" } }
+                { 1, new Person { Fio = "Петр", Date = DateTime.Parse("Jan 1, 2009"), PlaceOfBirth = "Западный", PassportNumber = 123, PlaceOfWork = "Cтроитель" } }
             };
             Console.WriteLine("Введите Fio (Петр) ");
             string fio = Console.ReadLine();
             Console.WriteLine("Введите дату рождения (Jan 1, 2009) ");
-            DateTime date = DateTime.Parse(Console.ReadLine());
+            DateTime date = DateTime.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
             Console.WriteLine("Введите место жительства (Западный) ");
             string PlaceOfBirth = Console.ReadLine();
             Console.WriteLine("Введите номер пасспорта (123) ");
-            int PassportNumber = Convert.ToInt32(Console.ReadLine());
-            Person seach = new Person { Fio = fio, Date = date, PlaceOfBirt = PlaceOfBirth, PassportNumber = PassportNumber };
+            int passportNumber = Convert.ToInt32(Console.ReadLine());
+            Person seach = new Person { Fio = fio, Date = date, PlaceOfBirth = PlaceOfBirth, PassportNumber = passportNumber };
             foreach (KeyValuePair<int, Person> dict in persons)
             {
                 if (seach.Equals(dict.Value))

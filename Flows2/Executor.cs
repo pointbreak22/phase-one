77375b71b5b3cefcabe76bf5973078102c220e36
Влад Зброжек <pointbreak22@outlook.com>
2021-09-10 10:36:09 +0300

    Edit

---
+++ Flows2/Executor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Flows2
+{
+    internal class Executor : IJobExecutor              //Наследник
+    {
+        public CancellationTokenSource CancellationTokenSource { get; set; }
+        public CancellationToken Token { get { return CancellationTokenSource.Token; } set { } }
+        public ConcurrentQueue<Task> TaskQueue { get; set; }
+        public int Amount { get { return TaskQueue.Count(); } }
+
+        public void Start(int maxConcurrent)
+        {
+            while (!Token.IsCancellationRequested)
+            {
+                var semaphore = new Semaphore(maxConcurrent, maxConcurrent);
+                while (Amount >= maxConcurrent && !Token.IsCancellationRequested)
+                {
+                    semaphore.WaitOne();
+
+                    if (TaskQueue.TryPeek(out Task task))
+                    {
+                        TaskQueue.TryDequeue(out task);
+                        task.Start();
+                    }
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            CancellationTokenSource.Cancel();
+        }
+
+        public void Add(Action action)
+        {
+            TaskQueue.Enqueue(new Task(action));
+        }
+
+        public void Clear()
+        {
+            TaskQueue.Clear();
+        }
+    }
+}
